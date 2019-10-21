82776afe36900f2b4f721588e7e58b0f50bf57f7
Riyuzakii <riyuzakiiitk@gmail.com>
2019-10-21 16:24:15 +0530

    Add bar-plots and fix x-axis order

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import sys, os
 import numpy as np 
 import matplotlib.pyplot as plt
@@ -28,15 +29,40 @@ for root, dirs, files in sorted(os.walk(directory)):
 
 for key, value in sorted(d.iteritems()) :
 	print key
-	prefetchers = ['berti','bingo','bouquet','default','enhancing','multi-lop','next-line','pangloss','sangam','sangam++','t-skid']
+	prefetchers = ['default','next-line','berti','bingo','bouquet','enhancing','multi-lop','pangloss','sangam','sangam++','t-skid']
 	x = [1,2,3,4,5,6,7,8,9,10,11]
+	min_value = min(value)
+	max_value = max(value)
+	# avg_value = sum(value)/len(value)
+	min_value = min_value - 0.001
+	if key == '603':
+		# print min_value, max_value
+		# print value
+		min_value = min_value + 0.001 - 0.00001
+
+	if key == '621':
+		# print min_value, max_value
+		# print value
+		min_value = 0.56175
+	if key == '648':
+		print min_value, max_value
+		print value
+		min_value = 1.6117	
+	# min_value = round(min_value, 2) - 0.001
+	# max_value = max_value + 0.01
+	# print min_value
 	fig = plt.figure()
 	ax = plt.axes()
+	# axes = plt.gca()
+	# axes.set_xlim([xmin,xmax])
+	ax.set_ylim([min_value,max_value])
 	plt.xticks(x, prefetchers,rotation=30, fontsize=8)
-	plt.plot(x, value)
+	# plt.yticks(np.arange(min_value, max_value, 0.001))
+	plt.bar(x, value)
 	plt.xlabel('Prefetchers from DPC3',fontsize=14)
 	plt.ylabel('Cumulative IPC',fontsize=14)
 	title = "Benchmark number: "+key
 	fig.suptitle(title,fontsize=15)
 
 	plt.savefig(key+'.pdf',bbox_inches = "tight")
+	# sys.exit(0)
